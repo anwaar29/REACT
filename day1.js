2eda9ec42dbbc2a4e75d75873e255374d593920f
anwaar29 <97968236+anwaar29@users.noreply.github.com>
2022-04-26 08:05:36 -0700

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,20 @@
+const App=()=>{
+  const [data, setData]=useState(0);
+  const increment=()=>{
+          setData(data+1)
+  }
+        return(
+          // <div className={style.main}>
+          //   <h1>
+          //     Hi There!
+          //   </h1>
+          // </div>
+          <div>
+            <h1>{data}</h1>
+          
+          <button onClick={increment}>click</button>
+          </div>
+
+        )
+}
+export default app
